5307605fb7d81d7212a43efe4fe7e7fc2e3cb832
akbarrmn <akbar.rahmana@gmail.com>
2022-06-15 15:53:56 +0700

    Push Detail Product Seller Pages

---
+++ frontend/src/pages/DetailProductPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbars from '../components/header/Navbars'
+import ProductDetails from '../components/detailproduct/ProductDetails'
+
+const DetailProductPage = () => {
+  return (
+    <>
+        <Navbars/>
+        <ProductDetails/>
+    </>
+  )
+}
+
+export default DetailProductPage
