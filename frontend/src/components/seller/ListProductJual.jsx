687b09367ac16ba2efecf9d43031411c2ea7737e
ariaseno1 <ariaseno.an@gmail.com>
2022-06-16 14:20:07 +0700

    fixing pages daftarJual

--- frontend/src/components/seller/ListProductJual.jsx
+++ frontend/src/components/seller/ListProductJual.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 const ListProductJual = () => {
   return (
     <>
-        <Box p={2} sx={{boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.25)', borderRadius: '16px' }}>
-            <Grid container rowSpacing={3} columnSpacing={{ xs: 3, sm: 3, md: 3 }} overflow='hidden' justifyContent={'start'}>
+        <Box p={2} sx={{boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.25)', borderRadius: '16px', overflow: 'hidden' }}>
+            <Grid container rowSpacing={3} columnSpacing={{ xs: 3, sm: 3, md: 3 }} overflow='hidden' >
                 <Grid item xs={3} sm={3} md={4} >
                     <Card sx={{ height: '100%' }} >
                         <CardActionArea sx={{ height: '100%', border: '1px dashed #bbb' }}>
@@ -160,30 +160,6 @@ const ListProductJual = () => {
                         </CardActionArea>
                     </Card>
                 </Grid>
-                <Grid item xs={3} sm={3} md={4} >
-                    <Card >
-                        <CardActionArea>
-                            <CardMedia
-                                component="img"
-                                height="140"
-                                image="/images/Product.png"
-                                alt="green iguana"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h6" component="div">
-                                    Jam Tangan
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    Aksesoris
-                                </Typography>
-                                <Typography gutterBottom variant="h6" component="div">
-                                    Rp.10.000,00
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                
                 
                 
                 
