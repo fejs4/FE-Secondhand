fa5a26002af700e0f210044df1416f035ac94f18
ariaseno1 <ariaseno.an@gmail.com>
2022-06-17 00:11:17 +0700

    add modals notif

--- frontend/src/components/header/Navbars.jsx
+++ frontend/src/components/header/Navbars.jsx
@@ -1,4 +1,4 @@
-import { Box, Button } from '@mui/material'
+import { Box, Button, IconButton, Menu } from '@mui/material'
 import React from 'react'
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
@@ -6,9 +6,40 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchField from './SearchField';
 import LoginIcon from '@mui/icons-material/Login';
+import Notifications from '../modals/Notifications';
 
 const Navbars = () => {
     const [user, setUser] = React.useState('a')
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const isNotifOpen = Boolean(anchorEl);
+
+    const handleNotifOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleNotifClose = () => {
+        setAnchorEl(null);
+    };
+
+    const renderMenu = (
+        <Menu
+          PaperProps={{sx: {width: '30%'}}}
+          sx={{ top: "50px" }}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={isNotifOpen}
+          onClose={handleNotifClose}
+        >
+            <Notifications/>
+        </Menu>
+      );
     return (
         <>
             <Box component={'div'} height={'3rem'} display={'flex'} sx={{ px: { xs: 5, sm: 7, md: 15 }, py: 2, boxShadow: { xs: 'none', md: '0px 0px 10px rgba(0, 0, 0, 0.15)' } }} justifyContent={'space-between'} alignItems={'center'} position={{ md: 'unset', xs: 'relative' }}>
@@ -33,15 +64,28 @@ const Navbars = () => {
                     <Box display={{ xs: 'none', md: 'flex' }} gap={3}>
                         {user ?
                             <>
-                                <FormatListBulletedIcon sx={{ cursor: 'pointer' }} />
-                                <NotificationsNoneOutlinedIcon sx={{ cursor: 'pointer' }} />
-                                <PersonOutlineOutlinedIcon sx={{ cursor: 'pointer' }} />
+                                <IconButton>
+                                    <FormatListBulletedIcon sx={{ cursor: 'pointer' }} />
+                                </IconButton> 
+                                <IconButton
+                                    edge="end"
+                                    aria-label="account of current user"
+                                    aria-haspopup="true"
+                                    onClick={handleNotifOpen}
+                                    color="inherit"
+                                >
+                                    <NotificationsNoneOutlinedIcon sx={{ cursor: 'pointer' }} />
+                                </IconButton> 
+                                <IconButton>
+                                    <PersonOutlineOutlinedIcon sx={{ cursor: 'pointer' }} />
+                                </IconButton> 
                             </>
                             :
                             <Button color='primary' variant='contained' sx={{ borderRadius: '12px', height: '48px', width: '105px' }}><LoginIcon sx={{ mr: 1 }} />Masuk</Button>
                         }
                     </Box>
                 </Box>
+                {renderMenu}
             </Box>
         </>
     )
