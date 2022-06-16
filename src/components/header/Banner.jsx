870c31b771dfefc91da216e0ac794000dfc9b06d
akbarrmn <akbar.rahmana@gmail.com>
2022-06-16 16:14:49 +0700

    fixing banner

--- src/components/header/Banner.jsx
+++ src/components/header/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
     <>
       <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'} width={'100%'} height={'288px'} mt={{ xs:0,md:3 }} mb={{ md:5, xs:-5 }} columnGap={3} top={{ md:'unset', xs:0 }} position={{ md:'unset', xs:'absolute'}} zIndex={{ md:'unset', xs:-1 }}>
         <Box display={{ xs: 'none', md: 'block' }} sx={{ width: '236px', height: '224px', backgroundColor: '#B6D4A8', borderRadius: '0 20px 20px 0' }}/>
-        <Box width={'100%'} height={'288px'} display={'flex'} justifyContent={'space-between'} alignItems={'center'} sx={{ background:'#FFE9CA', borderRadius:{md:'20px', xs:'unset'} }}>
+        <Box width={'100%'} height={{ md:'288px', xs:'390px' }} display={'flex'} justifyContent={'space-between'} alignItems={'center'} sx={{ background:{md:'#FFE9CA', xs:'linear-gradient(180deg, #FFE9C9 59.55%, rgba(255, 233, 202, 0) 100%)'}, borderRadius:{md:'20px', xs:'unset'} }}>
             <Box width={{ xs:'100%', md:'50%', lg:'50%' }} padding={{ lg:5, md:7, sm:7, xs:5 }}>
               <Typography variant='h4' fontWeight={700} sx={{ fontSize:{lg:'2rem', xs:'1.5rem'} }} my={1}>Bulan Ramadhan</Typography>
               <Typography variant='h4' fontWeight={700} sx={{ fontSize:{md:'2rem', xs:'1.5rem'} }} my={1}>Banyak Diskon!</Typography>
