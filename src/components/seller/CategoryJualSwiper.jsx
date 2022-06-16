a4fb6e23744f0fd0dc9bf157a03ae504a592603f
ariaseno1 <ariaseno.an@gmail.com>
2022-06-16 15:43:18 +0700

    add category swiper

--- src/components/seller/CategoryJualSwiper.jsx
+++ src/components/seller/CategoryJualSwiper.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Box, Button } from '@mui/material'
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+import { Swiper, SwiperSlide } from "swiper/react";
+
 
 const CategoryJualSwiper = () => {
+  const [clicked, setClicked] = React.useState('Semua');
+  let items = [];
+  
+ 
   return (
-    <div>CategoryJualSwiper</div>
+    <Box mt={3}>
+      <Swiper
+        slidesPerView={"auto"}
+        className="mySwiper"
+      >
+        <SwiperSlide className='filter-button'><Button className={'produk' === clicked ? 'button-active button-controll' : 'button-controll'} onClick={()=>setClicked('produk')}><ListAltIcon/>Produk </Button></SwiperSlide>
+        <SwiperSlide className='filter-button'><Button className={'diminati' === clicked ? 'button-active button-controll' : 'button-controll'} onClick={()=>setClicked('diminati')}><FavoriteIcon/>Diminati </Button></SwiperSlide>
+        <SwiperSlide className='filter-button'><Button className={'terjual' === clicked ? 'button-active button-controll' : 'button-controll'} onClick={()=>setClicked('terjual')}><MonetizationOnOutlinedIcon/>Terjual </Button></SwiperSlide>
+      </Swiper>
+    </Box>
   )
 }
 
