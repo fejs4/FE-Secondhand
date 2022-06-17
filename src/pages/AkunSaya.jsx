73e2edc77531b63d277e211b12212f4297af0210
ariaseno1 <ariaseno.an@gmail.com>
2022-06-17 20:58:05 +0700

    add pages AkunSaya

---
+++ src/pages/AkunSaya.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AkunSayaMobile from '../components/header/AkunSayaMobile'
+
+const AkunSaya = () => {
+  return (
+    <>
+        <AkunSayaMobile/>
+    </>
+  )
+}
+
+export default AkunSaya
