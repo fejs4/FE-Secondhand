211a49dd9dee09befbd8ac8a12fbfe4e0488f6d6
akbarrmn <akbar.rahmana@gmail.com>
2022-06-21 17:16:11 +0700

    Adding style

--- src/components/infopenawar/DataPenawar.jsx
+++ src/components/infopenawar/DataPenawar.jsx
@@ -8,6 +8,19 @@ import CloseIcon from '@mui/icons-material/Close';
 import BuyerInfo from '../detailproduct/BuyerInfo';
 import CircleIcon from '@mui/icons-material/Circle';
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '360px',
+    height: '438px',
+    bgcolor: 'background.paper',
+    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.15)',
+    borderRadius: '16px',
+    p: 4,
+};
+
 const DataPenawar = () => {
     const [open, setOpen] = React.useState(false)
     const [error, setError] = React.useState(false);
@@ -70,7 +83,7 @@ const DataPenawar = () => {
                                     </Button>
                                 </Grid>
                                 <Grid item xs={3}>
-                                    <Button fullWidth variant="contained" color="primary" sx={{ height: '40px', borderRadius: '25px' }}>
+                                    <Button fullWidth variant="contained" color="primary" onClick={handleOpen} sx={{ height: '40px', borderRadius: '25px' }}>
                                         Terima
                                     </Button>
                                 </Grid>
@@ -82,64 +95,53 @@ const DataPenawar = () => {
                 </Box>
 
             </Toolbar>
-        </Box>
-                        <Grid item xs={3}>
-                                <Button fullWidth variant="contained" color="primary"  onClick={handleOpen} sx={{ height: '40px', borderRadius:'25px'  }}>
-                                    Terima
-                                </Button>
-                        </Grid>
-                    </Grid>
-        </Grid>
-        </Box> 
 
-                </Box>
 
-                </Toolbar>
-                <Modal
-                    open={open}
-                    aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description"
-                >
-                    <Box sx={style}>
-                        <Box display={'flex'} justifyContent={'flex-end'}>
-                            <CloseIcon onClick={handleClose} sx={{ cursor: 'pointer' }} />
-                        </Box>
-                        <Typography id="modal-modal-description" variant='h6' sx={{ mt: 2, fontSize: '14px', fontWeight: 700 }}>
-                            Yeay kamu berhasil mendapat harga yang sesuai
-                        </Typography>
-                        <Typography sx={{ mt: 2, fontSize: '14px', color: '#8A8A8A' }}>
-                            Segera hubungi pembeli melalui WhatsApp untuk transaksi selanjutnya
-                        </Typography>
-                        <Box display={'flex'} fullWidth sx={{ mt:3,background: '#EEEEEE',padding:2, height: '150px', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.15)' }}>
-                            <Box component={'img'} src='/images/Buyer.png' sx={{ height: '48px' }} />
-                            <Box display={'flex'} flexDirection={'column'} ml={2}>
-                                <Typography>Nama Pembeli</Typography>
-                                <Typography variant="caption" color='text.secondary'  component="h2" >Kota</Typography>
-                                {/* <BuyerInfo/> */}
-                            </Box>
-                            <Grid >
-                            <Box component={'img'} src='/images/Product.png' sx={{ height:'48px' , borderRadius:'16px'}}/>
-                            </Grid>
-                            <Grid item xs={6}>
-                                <Typography variant='subtitle1' fontWeight={550}  my={0} >
-                                    Jam Tangan Casio
-                                </Typography> 
-                                <Typography variant='subtitle1' fontWeight={550}  my={0} >
-                                    Rp 250.000
-                                </Typography>
-                                <Typography variant='subtitle1' fontWeight={550}  my={0} >
-                                    Ditawar Rp 200.000
-                                </Typography>
-                            </Grid>
-                        </Box>
-                        <Button onClick={handlePost} variant='contained' fullWidth color='primary' sx={{ borderRadius: '16px', height: '48px',marginTop:2 }}>
-                            Hubungi via WhatsApp
-                        </Button>
-                        
+            <Modal
+                open={open}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Box display={'flex'} justifyContent={'flex-end'}>
+                        <CloseIcon onClick={handleClose} sx={{ cursor: 'pointer' }} />
                     </Box>
-                    
-                </Modal>
+                    <Typography id="modal-modal-description" variant='h6' sx={{ mt: 2, fontSize: '14px', fontWeight: 700 }}>
+                        Yeay kamu berhasil mendapat harga yang sesuai
+                    </Typography>
+                    <Typography sx={{ mt: 2, fontSize: '14px', color: '#8A8A8A' }}>
+                        Segera hubungi pembeli melalui WhatsApp untuk transaksi selanjutnya
+                    </Typography>
+                    <Box display={'flex'} fullWidth sx={{ mt: 3, background: '#EEEEEE', padding: 2, height: '150px', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.15)' }}>
+                        <Box component={'img'} src='/images/Buyer.png' sx={{ height: '48px' }} />
+                        <Box display={'flex'} flexDirection={'column'} ml={2}>
+                            <Typography>Nama Pembeli</Typography>
+                            <Typography variant="caption" color='text.secondary' component="h2" >Kota</Typography>
+                            {/* <BuyerInfo/> */}
+                        </Box>
+                        <Grid >
+                            <Box component={'img'} src='/images/Product.png' sx={{ height: '48px', borderRadius: '16px' }} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography variant='subtitle1' fontWeight={550} my={0} >
+                                Jam Tangan Casio
+                            </Typography>
+                            <Typography variant='subtitle1' fontWeight={550} my={0} >
+                                Rp 250.000
+                            </Typography>
+                            <Typography variant='subtitle1' fontWeight={550} my={0} >
+                                Ditawar Rp 200.000
+                            </Typography>
+                        </Grid>
+                    </Box>
+                    <Button onClick={handlePost} variant='contained' fullWidth color='primary' sx={{ borderRadius: '16px', height: '48px', marginTop: 2 }}>
+                        Hubungi via WhatsApp
+                    </Button>
+
                 </Box>
+
+            </Modal>
+        </Box >
     )
 }
 export default DataPenawar
