4f2f0c14ebb72e0ed7d987ccd723650aa0820bd2
ariaseno1 <ariaseno.an@gmail.com>
2022-06-25 01:34:18 +0700

    responsive page info-penawar

--- src/components/detailproduct/BuyerInfo.jsx
+++ src/components/detailproduct/BuyerInfo.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const BuyerInfo = () => {
   return (
     <>
-        <Box component={'div'} rowGap={2} p={4} display={'flex'} mt={3} sx={{ boxShadow:'0px 0px 4px rgba(0, 0, 0, 0.15)', borderRadius:'16px'}}>
+        <Box component={'div'} rowGap={2} p={{ xs:2, md:4 }} display={'flex'} mt={3} sx={{ boxShadow:'0px 0px 4px rgba(0, 0, 0, 0.15)', borderRadius:'16px'}}>
             <Box component={'img'} src='/images/Buyer.png' sx={{ height:'48px' }}/>
             <Box display={'flex'} flexDirection={'column'} ml={2}>
                 <Typography>Nama Pembeli</Typography>
