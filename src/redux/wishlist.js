02b121fa7e5a20274a2d4bf00d7a4faa9381daa5
akbarrmn <akbar.rahmana@gmail.com>
2022-06-28 21:30:09 +0700

    Setup redux wishlist

--- src/redux/wishlist.js
+++ src/redux/wishlist.js
@@ -1,3 +1,44 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+export const fetchWishlist = createAsyncThunk(
+    'wishlist/wishlist',
+    async () => {
+        
+    }
+);
+
+const initialState = {
+    loading: false,
+    error: null,
+    wishlist: {}
+}
+
+const productSlice = createSlice({
+    name: 'wishlist',
+    initialState,
+    reducers: {
+        setLoading: (state, action) => {
+            state.loading = action.payload
+        },
+    },
+    extraReducers: {
+
+        // Fetching Wishlist
+        [fetchWishlist.pending]: (state, action) => {
+            console.log('pending')
+            return { ...state, loading: true, error: null, }
+        },
+        [fetchWishlist.fulfilled]: (state, action) => {
+            console.log('fulfilled')
+            return { ...state, wishlist: action.payload }
+        },
+        [fetchWishlist.rejected]: (state, action) => {
+            console.log('rejected')
+            return { ...state, error: action.error }
+        },
+
+    }
+})
+export const { setLoading } = productSlice.actions;
+export default productSlice.reducer;
