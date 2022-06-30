9a7d2fb53c8158c851db86508938832bbeaa5bf9
akbarrmn <akbar.rahmana@gmail.com>
2022-06-30 14:08:56 +0700

    Adding feature search product

--- src/components/products/ItemCard.jsx
+++ src/components/products/ItemCard.jsx
@@ -2,7 +2,7 @@ import { Box, Card, CardActionArea, CardContent, CardMedia, Grid, Typography } f
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { fetchProducts, setLoading } from '../../redux/product'
+import { fetchProducts, setDetail, setLoading } from '../../redux/product'
 import Skeleton from '@mui/material/Skeleton';
 import CardLoading from '../loading/CardLoading'
 import axios from 'axios'
@@ -12,13 +12,14 @@ const ItemCard = ({clicked}) => {
     const formatter = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" })
     const data = useSelector(state => state.product.products)
     const loading = useSelector(state => state.product.loading)
-
+    const searched = useSelector(state => state.product.searched)
     React.useEffect(() => {
-        dispatch(fetchProducts(clicked))
+        dispatch(setDetail({}))
+        dispatch(fetchProducts({clicked,searched}))
         setTimeout(() => {
             dispatch(setLoading(false))
         }, 1500);
-    }, [clicked])
+    }, [clicked,searched])
     return (
         <>
             <Grid container rowSpacing={3} columnSpacing={{ xs: 3, sm: 3, md: 3 }} mt={3}>
@@ -35,8 +36,8 @@ const ItemCard = ({clicked}) => {
                                                         component="img"
                                                         height="140"
                                                         image={item.images ? `https://be-kel1.herokuapp.com/public/images/${item.images[0]}` : ''}
-                                                        alt="green iguana"
-                                                        sx={{ objectFit: 'cover' }}
+                                                        alt="pic"
+                                                        sx={{ objectFit: 'contain' }}
                                                     />
                                                     <CardContent>
                                                         <Typography gutterBottom variant="h6" component="div" sx={{ fontSize: { xs: '1.1em', md: '1.5em' } }}>
@@ -45,7 +46,7 @@ const ItemCard = ({clicked}) => {
                                                         <Typography variant="body2" color="text.secondary" mb={1} sx={{ fontSize: { xs: '0.9em', md: '1.2em' } }}>
                                                             {item.category ? item.category : ''}
                                                         </Typography>
-                                                        <Typography gutterBottom variant="h6" component="div" sx={{ fontSize: { xs: '1.em', md: '1.3em' } }}>
+                                                        <Typography gutterBottom variant="h6" component="div" sx={{ fontSize: { xs: '.8em', md: '1.3em' } }}>
                                                             {item.price ? formatter.format(item.price) : ''}
                                                         </Typography>
                                                     </CardContent>
